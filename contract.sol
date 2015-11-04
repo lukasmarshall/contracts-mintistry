6ba9ef8e7bf376b79526f02cdc4d16d47f60e0d3
lukasmarshall <lukasmarshall@gmail.com>
2015-11-04 12:25:08 -0800

    Fixed errors in Solidity contract
    I renamed balance to ‘bal’ and ‘address’ in address approval to ‘addr’
    - hope this still complies with metacoin.

--- contract.sol
+++ contract.sol
@@ -3,25 +3,25 @@ Based on Ethereum tutorial material and API outlined at:
 https://github.com/ethereum/wiki/wiki/Standardized_Contract_APIs
 */
 contract customCoin { 
-  mapping (address => uint) public balance;
+  mapping (address => uint) public bal;
   event Transfer(address indexed from, address indexed to, uint256 value);
-  event AddressApproval(address indexed address, address indexed proxy, bool result);
-  event AddressApprovalOnce(address indexed address, address indexed proxy, uint256 value);
+  event AddressApproval(address indexed addr, address indexed proxy, bool result);
+  event AddressApprovalOnce(address indexed addr, address indexed proxy, uint256 value);
   address public issuer;
   bytes32 public name;
 
   /* Initialize contract with initial supply tokens to the coin issuer */
   function customCoin(uint supply, address coin_issuer, bytes32 coin_name) {
-    balance[coin_issuer] = supply;
+    bal[coin_issuer] = supply;
     issuer = coin_issuer;
     name = coin_name;
   }
 
   /* Simple coin sending function */
   function transfer(address _to, uint256 _value) returns(bool success) {
-    if (balance[msg.sender] < _value) return false;
-    balance[msg.sender] -= _value;
-    balance[_to] += _value;
+    if (bal[msg.sender] < _value) return false;
+    bal[msg.sender] -= _value;
+    bal[_to] += _value;
     Transfer(msg.sender, _to, _value);
     return true;
   }
@@ -30,16 +30,16 @@ contract customCoin {
   For the moment we will not allow account authorisation so this will function the same as the transfer function above.*/
   function transferFrom(address _from, address _to, uint256 _value) returns(bool success) {
     if (msg.sender != _from) return false;
-    if (balance[_from] < _value) return false;
-    balance[_from] -= _value;
-    balance[_to] += _value;
+    if (bal[_from] < _value) return false;
+    bal[_from] -= _value;
+    bal[_to] += _value;
     Transfer(_from, _to, _value);
     return true;
   }
 
-  /*Return the balance of a given address - can already be used with balance() but complying with API standard here.*/
+  /*Return the bal of a given address - can already be used with balance() but complying with API standard here.*/
   function balanceOf(address _address) constant returns (uint256 balance){
-    return balance[_address];
+    return bal[_address];
   }
 
   /*The following functions are set to return false as in this implementation 
